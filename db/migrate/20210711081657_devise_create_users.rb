90de86fc1087c9eb59f89ec73b3bbabebe190562
zigzag826 <86278987+zigzag826@users.noreply.github.com>
2021-07-11 17:56:55 +0900

    devise_userテーブルの作成

--- db/migrate/20210711081657_devise_create_users.rb
+++ db/migrate/20210711081657_devise_create_users.rb
@@ -6,6 +6,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :name,               null: false
+      t.text   :profile,            null: false
+      t.text   :occupation,         null: false
+      t.text   :position,           null: false
 
       ## Recoverable
       t.string   :reset_password_token
